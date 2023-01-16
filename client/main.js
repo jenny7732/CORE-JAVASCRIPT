a598f48a68e4afa000dda408e0e9f0061c66537b
jenny7732 <jenny7732@naver.com>
2023-01-16 10:10:41 +0900

    클립보드기능 추가

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 
-import {clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString,addClass,removeClass,showAlert} from './lib/index.js'
+import {clearContents, getInputValue, getNode, getRandom, insertLast, isNumericString,addClass,removeClass,showAlert,copy} from './lib/index.js'
 
 import { jujeobData } from "./data/data.js";
 
@@ -19,7 +19,16 @@ function clickSubmitHandler(e){
   
   if(!name){
     console.log('이름을 입력해 주세요!');
-    showAlert('.alert-error','잘못된 정보입니다.!',2000);
+    showAlert('.alert-error','정확한 이름을 입력해주세요!',2000);
+
+    //GSAP
+    gsap.fromTo(resultArea, 0.01, {x:-5}, {x:5, clearProps:"x", repeat:20})
+
+    /* addClass(resultArea, 'shake');
+    setTimeout(()=>{
+      removeClass(resultArea,'shake');
+    },1000); */
+
     return 
   }
 
@@ -34,9 +43,18 @@ function clickSubmitHandler(e){
   
 }
 
-
+function clickCopyHandler(){
+  let text = resultArea.textContent;
+  //navigator.clipboard.writeText(text);
+  //promise
+  copy(text).then(()=>{
+    showAlert('.alert-success', '클릅보드 복사가 완료됐습니다.', 2000)
+  })
+  
+}
 
 submit.addEventListener('click',clickSubmitHandler)
+resultArea.addEventListener('click', clickCopyHandler);
 
 
 
