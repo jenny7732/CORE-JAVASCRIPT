6aa3b29fd4efe73928c49013a1d0b0445114a232
jenny7732 <jenny7732@naver.com>
2023-01-16 16:09:53 +0900

    메모이제이션 기능 추가

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 
-import { diceAnimation , disableElement, enableElement, getNode, getNodes, visibleElement, invisibleElement} from "./lib/index.js";
+import { attr, clearContents, diceAnimation , disableElement, enableElement, getNode, getNodes, visibleElement, invisibleElement, insertLast, memo} from "./lib/index.js";
 
 /* [주사위 굴리기]
 1. dice 애니메이션 불러오기
@@ -14,6 +14,17 @@ import { diceAnimation , disableElement, enableElement, getNode, getNodes, visib
 4. visible 활성 유틸 함수 만들기
 5. toggleState 유틸 함수 만들기 */
 
+// [ 레코드 템플릿 뿌리기 ]
+// 1. renderRecordListItem 함수 만들기
+// 2. HTML 템플릿 만들기
+// 3. 템플릿 뿌리기 
+
+// [ 초기화 시키기 ]
+// 1. clearContent 로 정보 지우기
+// 2. total, count 초기화 
+// 3. 스크롤 밑으로 보내기 
+// 4. 메모이제이션 패턴 
+
 //유사 배열, 배열의 구조 분해 할당
 const [rollingDiceButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
 const recordListWrapper = getNode('.recordListWrapper');
@@ -21,9 +32,35 @@ const recordListWrapper = getNode('.recordListWrapper');
 const recordButton = getNode('.buttonGroup > button:nth-child(2)')
 const resetButton = getNode('.buttonGroup > button:nth-child(2)') */
 
+memo('@tbody',()=>getNode('.recordListWrapper tbody'));
+
+/* render */
+
+let count = 0;
+let total = 0;
+// redux
+// mobx
+
+function renderRecordListItem(){
+  
+  let diceValue = Number(attr(memo('cube'),'data-dice'));
+
+  let template = /* html */ `
+    <tr>
+      <td>${++count}</td>
+      <td>${diceValue}</td>
+      <td>${total += diceValue}</td>
+    </tr>
+  `
+  
+  insertLast(memo('@tbody'),template)
+  recordListWrapper.scrollTop = recordListWrapper.scrollHeight
+}
+
+/* event */
+
 //IIFE
 //(함수)() or (함수()) -> 즉각실행함수
-
 const handleRollingDice = (() => {
   let stopAnimation;
   let isRolling = false;
@@ -45,12 +82,22 @@ const handleRollingDice = (() => {
   }
 })()
 
+
 const handleRecord = ()=>{
+
+  renderRecordListItem()
   visibleElement(recordListWrapper) //보여주기
+
+  
 }
 
 const handleReset = () => {
+
+  count = 0;
+  total = 0;
+
   invisibleElement(recordListWrapper) //숨기기
+  clearContents('.recordListWrapper tbody')
 }
 
 rollingDiceButton.addEventListener('click', handleRollingDice)
